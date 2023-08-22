7df35f4beeb1f7e428227ca7729e84118ab9cc05
satoshi0122 <rego.satosi@gmail.com>
2023-08-22 22:49:08 +0900

    breeze削除

--- camp-pro/routes/web.php
+++ camp-pro/routes/web.php
@@ -19,17 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
 
 Route::resource('mst_sites',Mst_SiteController::class)->except([
     'destroy'
