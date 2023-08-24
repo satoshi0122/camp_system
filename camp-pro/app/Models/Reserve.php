5412343303b3223d9fd766dc0077ebfea6132844
satoshi0122 <rego.satosi@gmail.com>
2023-08-24 21:38:33 +0900

    reserveのマイグレーションファイル追加

--- camp-pro/app/Models/Reserve.php
+++ camp-pro/app/Models/Reserve.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reserve extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['user_id','site_id','people_num','started','finished','canceled','deleted_at'];
+
+        // userが親の一対多リレーション
+        public function users(){
+            return $this->hasMany('User');
+        }
+
+        public function sites(){
+            return $this->hasMany('Site');
+        }
 }
--- camp-pro/database/migrations/2023_07_26_133918_create_reserves_table.php
+++ camp-pro/database/migrations/2023_07_26_133918_create_reserves_table.php
@@ -15,7 +15,17 @@ return new class extends Migration
     {
         Schema::create('reserves', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('site_id');
+            $table->string('people_num', 10)->nullable(false);
+            $table->dateTime('started')->nullable(false);
+            $table->dateTime('finished')->nullable(false);
             $table->timestamps();
+            $table->dateTime('canceled');
+            $table->datetime('deleted_at')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('site_id')->references('id')->on('mst_sites');
         });
     }
 
